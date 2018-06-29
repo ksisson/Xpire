6d0e42dc79dfe7f5d93bf75dcc261da259b97744
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-29 16:35:14 -0700

    deleted empty files that were messing everything up

--- models/login.js
+++ models/login.js
@@ -1,31 +1,4 @@
 module.exports = function(sequelize, DataTypes) {
-<<<<<<< HEAD
-  var login = sequelize.define("login", {
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    token: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate:{
-        len: [1]
-      }
-    }
-  });
-  return login;
-};
-=======
     var login = sequelize.define("login", {
       username: {
         type: DataTypes.STRING,
@@ -41,14 +14,20 @@ module.exports = function(sequelize, DataTypes) {
           len: [1]
         }
       },
+      token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate:{
+          len: [1]
+        }
+      }
     });
 
-    login.associate = function(models){
-        login.belongsToMany(models.api, {
-            through: "mastertable"
-        });
-    };
+    // login.associate = function(models){
+    //     login.belongsToMany(models.api, {
+    //         through: "mastertable"
+    //     });
+    // };
 
     return login;
    };
->>>>>>> cb69cf8f241365430b61647606d950f0d4193f2f
