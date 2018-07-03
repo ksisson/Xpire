9270c57f3d0aa72fd42b47d6a4777205e7b31226
Kyle Sisson <kylegsisson@gmail.com>
2018-07-03 12:00:40 -0700

    beautified

--- public/assets/js/welcome.js
+++ public/assets/js/welcome.js
@@ -1,84 +1,81 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
     var user_id = parseInt($("#user").attr("userId"));
-    $("#submitButton").on("click", function(){
+    $("#submitButton").on("click", function () {
         var food_name = $("#foodItem").val().trim()
         // var user_id = parseInt($("#user").attr("userId"));
         console.log(user_id);
-        
-        $.get("/api/apis/" + food_name, function(data){
-            
-            console.log("Data: ");
-            console.log(data);
-            
-           
-            //if items is not in the database 
-        if(data.length === 0){
-           
-            $("#results-modal").modal("toggle");    
-        }
-        else if(data[0].custom === true){
-            $("#results-modal").modal("toggle");
-        }
-         else{
-            var master_record = {
-                            loginId : user_id,
-                            apiId: data[0].id}
-        
-                        $.post("/api/mastertable", master_record).then(function(results){
-                            console.log(results)
-                           
-                            location.reload(true);
-                            $("#foodItem").val("")
-                        }).fail(function(){alert("This product is already on your list!");
-                                      });
 
-             
-         }
-           
-           
-          
+        $.get("/api/apis/" + food_name, function (data) {
+
+            //if items is not in the database 
+            if (data.length === 0) {
+
+                $("#results-modal").modal("toggle");
+            } else if (data[0].custom === true) {
+                $("#results-modal").modal("toggle");
+            } else {
+                var master_record = {
+                    loginId: user_id,
+                    apiId: data[0].id
+                }
+
+                $.post("/api/mastertable", master_record).then(function (results) {
+                    console.log(results)
+
+                    location.reload(true);
+                    $("#foodItem").val("")
+                }).fail(function () {
+                    alert("This product is already on your list!");
+                });
+
+
+            }
+
+
+
         });
-        
+
     });
 
-    $("#modalSubmit").on("click", function(){
+    $("#modalSubmit").on("click", function () {
         var food_name = $("#foodItem").val().trim()
         // var user_id = parseInt($("#user").attr("userId"));
         var shelflife = parseInt($("#shelflife").val().trim())
         var category = $("#category").val()
-        
+
 
         var api_addition = {
             item_name: food_name,
             shelf_life: shelflife,
             category: category,
             custom: true,
-            user_id: user_id}
-            
+            user_id: user_id
+        }
 
-        $.post("/api/apis", api_addition, function(data){
+
+        $.post("/api/apis", api_addition, function (data) {
             console.log(data)
             var master_record = {
                 loginId: user_id,
-                apiId: data.id}
+                apiId: data.id
+            }
 
-            $.post("/api/mastertable", master_record).then(function(results){
+            $.post("/api/mastertable", master_record).then(function (results) {
                 console.log(results);
                 location.reload(true);
-              
+
             })
         })
-        
+
     });
 
-    $(".delete").on("click", function(){
+    $(".delete").on("click", function () {
         var user_id = $(this).attr("loginId")
         var api_id = $(this).attr("apiId")
         $.ajax({
             type: "DELETE",
             url: "/api/" + user_id + "/15/" + api_id + "/23423",
-            success: function(data){
+            success: function (data) {
                 console.log(data)
                 location.reload()
             }
@@ -99,45 +96,43 @@ $(document).ready(function() {
 
 
 
-            // //check will be used if an item with custom value true has already been added to the user 
-            // var userCheck = false;
-            // console.log("current user id"+ user_id);
-            // for( var i = 0; i<data.length;i++){
-            //     console.log(data[i].user_id);
-            //     if(data[i].user_id ===user_id ){
-            //         console.log("check is changed")
-            //         userCheck=true;
-            //     }
-            // }
-            // if(userCheck===true){
-            //     alert("Item is already on your list!");
-            // }
-            // else{
-            //     if(data[0].custom ===false){
+// //check will be used if an item with custom value true has already been added to the user 
+// var userCheck = false;
+// console.log("current user id"+ user_id);
+// for( var i = 0; i<data.length;i++){
+//     console.log(data[i].user_id);
+//     if(data[i].user_id ===user_id ){
+//         console.log("check is changed")
+//         userCheck=true;
+//     }
+// }
+// if(userCheck===true){
+//     alert("Item is already on your list!");
+// }
+// else{
+//     if(data[0].custom ===false){
 
-            //         // var user_id = parseInt($("#user").attr("userId"));
-                    
-            //         var master_record = {
-            //             loginId : user_id,
-            //             apiId: data[0].id}
-    
-            //         $.post("/api/mastertable", master_record).then(function(results){
-            //             console.log(results)
-                       
-            //             location.reload(true);
-            //             $("#foodItem").val("")
-            //         }).fail(function(){
-            //             alert("This product is already on your list!");
-            //           });
-            //     }
-            //     else{
-            //     $("#results-modal").modal("toggle");
-            //     }
-            // }
-            
-            // //if a user's entry info was added to the database directly
-            
+//         // var user_id = parseInt($("#user").attr("userId"));
 
-            // //if a user has a specific item in their list, prevent from adding 
-                           
-        
+//         var master_record = {
+//             loginId : user_id,
+//             apiId: data[0].id}
+
+//         $.post("/api/mastertable", master_record).then(function(results){
+//             console.log(results)
+
+//             location.reload(true);
+//             $("#foodItem").val("")
+//         }).fail(function(){
+//             alert("This product is already on your list!");
+//           });
+//     }
+//     else{
+//     $("#results-modal").modal("toggle");
+//     }
+// }
+
+// //if a user's entry info was added to the database directly
+
+
+// //if a user has a specific item in their list, prevent from adding
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,4 +1,3 @@
-
 // *********************************************************************************
 // html-routes.js - this file offers a set of routes for sending users to the various html pages
 // *********************************************************************************
@@ -10,120 +9,119 @@ var db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // home route
 
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
 
-    
+
     //if user is already logged in  
-    if(req.session.user){
-      var userObject = { 
-          id: req.session.user.id,       
-          username: req.session.user.username
+    if (req.session.user) {
+      var userObject = {
+        id: req.session.user.id,
+        username: req.session.user.username
       };
       //retrieving all items from api array with the id of the session
       db.mastertable.findAll({
-        
-        where:
-        {
+
+          where: {
             loginId: req.session.user.id,
-      
-        }
-      })
-        .then(function(dbmastertable){
+
+          }
+        })
+        .then(function (dbmastertable) {
           var foodlist = [];
 
 
-          for (var i = 0; i < dbmastertable.length; i++){
-            foodlist.push(dbmastertable[i].dataValues) 
+          for (var i = 0; i < dbmastertable.length; i++) {
+            foodlist.push(dbmastertable[i].dataValues)
           }
           console.log("food list");
           console.log(foodlist);
 
           var count = 0;
-          
-          if(foodlist.length===0){
+
+          if (foodlist.length === 0) {
             console.log("length of food list is 0")
             res.render("welcome", userObject);
-          }else{
+          } else {
 
-            for (let i = 0; i < foodlist.length; i++){
-              db.api.findOne({where: {id: foodlist[i].apiId}})
-              .then(function(results){
-                count += 1;
-                // console.log(count, foodlist.length)
-                foodlist[i].name = results.dataValues.item_name
-                foodlist[i].expiration = getexpiration(results.dataValues.createdAt ,results.dataValues.shelf_life)
-                foodlist[i].shelflife = getshelflife(foodlist[i].expiration)
-                foodlist[i].formattedexpiration = formattedexpiration(foodlist[i].expiration)
-                if(count === foodlist.length){
-                  console.log("foodlist inside if: ");
-                  userObject.items = foodlist;
-                  console.log(userObject.items);
-                  res.render("welcome", userObject);
-                  
-                  
-                };
-                
-              });
+            for (let i = 0; i < foodlist.length; i++) {
+              db.api.findOne({
+                  where: {
+                    id: foodlist[i].apiId
+                  }
+                })
+                .then(function (results) {
+                  count += 1;
+                  // console.log(count, foodlist.length)
+                  foodlist[i].name = results.dataValues.item_name
+                  foodlist[i].expiration = getexpiration(results.dataValues.createdAt, results.dataValues.shelf_life)
+                  foodlist[i].shelflife = getshelflife(foodlist[i].expiration)
+                  foodlist[i].formattedexpiration = formattedexpiration(foodlist[i].expiration)
+                  if (count === foodlist.length) {
+                    console.log("foodlist inside if: ");
+                    userObject.items = foodlist;
+                    console.log(userObject.items);
+                    res.render("welcome", userObject);
+
+
+                  };
+
+                });
             }
 
-          }    
-        
-      });
-      
+          }
+
+        });
+
     }
     //if there is a cookie  
-    else if (req.cookie){
+    else if (req.cookie) {
       for (var i = 0; i < users.length; i++) {
-        if (users[i].token === req.cookie.token){
+        if (users[i].token === req.cookie.token) {
           req.session.user = users[i];
           return res.redirect("/");
         }
       }
     }
     //if not logged in
-    else{
+    else {
       res.render("index");
     }
   });
-  
+
 
 
 };
 
-Date.prototype.addDays = function(days){
+Date.prototype.addDays = function (days) {
   var date = new Date(this.valueOf());
   date.setDate(date.getDate() + days);
   return date;
 }
 
-function getexpiration(datestamp,shelflife){
+function getexpiration(datestamp, shelflife) {
   var expiration = datestamp.addDays(shelflife);
   return expiration
 }
 
-function getshelflife(expiration){
-  var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+function getshelflife(expiration) {
+  var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   var firstDate = new Date();
   var secondDate = expiration
 
-  var diffDays = Math.ceil(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+  var diffDays = Math.ceil(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay)));
   return diffDays;
 }
 
-function formattedexpiration(expiration){
+function formattedexpiration(expiration) {
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   var d = expiration;
   var formatted = days[d.getDay()] + " " + months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
   return formatted;
 }
-
-
-   
-
